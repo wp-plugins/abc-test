085fdce8d4c656a52a63f55d5779f550bf8ff4f0
webfaster <webfaster@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-27 20:22:54 +0000

    git-svn-id: https://plugins.svn.wordpress.org/abc-test/trunk@604830 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ abctest_config.php
@@ -0,0 +1,145 @@
+<?php 
+	
+	class abcTestConfig {
+		
+	private static $message;
+	private static $msgstatus;
+	
+		public static function adminarea()
+		{				
+			?>		
+			<div class="wrap">
+	
+			<div id="icon-options-general" class="icon32"><br /></div>		
+				
+			<h2>ABC Test: <?php _e('settings','abctest_lbl'); ?></h2>
+								
+							
+				<?php 
+	
+				// Azioni principali
+				
+				switch($_GET['do'])
+				{
+					
+		
+					case 'update':
+						
+						if ($_SERVER['REQUEST_METHOD'] == 'POST') {					
+							self::update();
+						}
+										
+					break;
+					
+					
+					default:
+						self::mainform('update');
+					break;
+					
+				}
+		
+			
+				?>			
+	
+					    <div id="message" class="<?php echo self::$msgstatus ?>">
+					    <p>
+					    	<?php echo self::$message ?>
+					    </p>
+					    </div>
+			
+			
+			</div>		
+			<?php
+			
+		}
+
+		
+		public static function update()
+		{
+			
+			$selTmpl = $_POST['template'];
+			update_option('selectedTemplate', $selTmpl);
+	
+			self::$message = __('The data have been updated.','abctest_lbl');
+			self::$msgstatus = 'updated';
+			
+			//self::mainform('');
+						
+		}
+		
+		
+		
+
+		public static function mainform($action)
+		{
+	
+			$actions = array('' => '', 
+							 'update' => '?page=config&do=update');	
+		
+		?>
+		
+		<form method="post" action="<?php echo $actions[$action]; ?>">
+	
+			<div id="poststuff" class="metabox-holder has-right-sidebar">
+	
+				<div class="stuffbox">
+				<h3><label for="test_title"><?php _e('Select look and feel','abctest_lbl') ?></label></h3>
+				<div class="inside">
+											
+					<select name="template" id="template" style="width: 300px;">
+			
+						<?php 
+						
+						
+						$selectedTemplate = get_option('selectedTemplate', 'default');
+							
+						$abspath = dirname( __FILE__ );
+							
+						$path = $abspath . '/templates';
+						$results = scandir($path);
+						
+						foreach ($results as $result) {
+						    if ($result === '.' or $result === '..') continue;
+						
+						    if (is_dir($path . '/' . $result)) {
+						      	
+						      		$SELECTED = ($selectedTemplate == $result) ? ' selected="selected"' : NULL;
+						      	
+						      	printf('<option%s>%s</option>', $SELECTED, $result);
+						    	echo "\n";
+						    	
+						    }
+						
+						}
+							
+						
+						?>						
+					</select> 				    
+				</div>
+				</div>
+
+							
+				<p>
+					<input type="hidden" name="irecord" value="<?php 
+						
+						$idi = mysql_real_escape_string( trim($_GET['id']) );
+																				
+						echo (int) $idi;
+						
+					?>" />
+					<input type="submit" name="submit" id="submit" class="button-primary" value="<?php _e('Save changes','abctest_lbl')  ?>"  />
+				</p>
+	
+		
+			</div><!-- #poststuff -->
+				
+		</form>	
+	
+		<?php			
+		}
+
+			
+
+	}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,45 @@
+=== ABC Test ===
+Contributors: webfaster
+Donate link: http://www.webfaster.it/cms/wordpress/plugins/abctest
+Tags: quiz, test
+Requires at least: 3.2.1
+Tested up to: 3.2.1
+Stable tag: 0.3
+
+ABC Test lets to present quizzes with three options for each question.
+ 
+== Description ==
+
+ABC Test lets to present quizzes with three options for each question.
+At the end of the quiz the user has a response based on the prevalence of answers A, B or C.
+
+No limit for questions in each quiz.
+
+Each quiz must contains four questions at least.
+
+
+== Installation ==
+
+1. Upload unzipped folder `abctest to` the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Put a shortcode in page or post editor: [abctest id="1"]
+
+Note: make sure the plugin folder is named 'abctest', not 'abc-test' or similar.
+
+
+== Frequently Asked Questions ==
+
+= Can I customize the look and feel of the quiz? =
+Yes, please refer to the folder 'templates / default'. Create a new folder with the same structure and a different name.
+You can edit html and css files as you wish. Then set the new theme from the admin panel: Configuration> Select the graphic theme.
+
+== Screenshots ==
+
+== Changelog ==
+
+= 0.1 =
+
+== Upgrade Notice ==
+
+= 0.3 =
+Security patch
